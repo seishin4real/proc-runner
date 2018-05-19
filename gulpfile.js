f00e976ab8923783cfd8117aa8647d3a6165cc73
Łukasz Sobków <seishin@o2.pl>
2018-05-19 12:22:26 +0200

    fix exe-build notifications not showing run scripts cleanup impr. build process impr. readme

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,32 @@
 const gulp = require('gulp');
 const copy = require('gulp-copy');
+const rename = require('gulp-rename');
+const del = require('del');
 
-gulp.task('build-electron-app', function () {
+gulp.task('prepare-app-sources', function () {
+  del.sync([
+    'dist-electron/dist',
+    'dist-electron/src',
+    'dist-electron/index.*'
+  ]);
   return gulp
-    //todo clean dist-electron/ except node_modules
-    .src(['dist/*', 'index.js', 'index.html', 'package.json', 'src/electron/store.electron.js', 'src/electron/toaster.electron.js', 'src/store.default.js'])
+    .src([
+      'dist/*',
+      'index.js',
+      'index.html',
+      'package.json',
+      'src/electron/store.electron.js',
+      'src/electron/toaster.electron.js',
+      'src/electron/toaster.html',
+      'src/electron/toaster.client.js',
+      'src/store.default.js'
+    ])
     .pipe(copy('dist-electron/'));
 });
+
+
+gulp.task('init-app-sources', function () {
+  return gulp.src('package.electron.json')
+    .pipe(rename('package.json'))
+    .pipe(gulp.dest('dist-electron/'));
+})
--- readme.md
+++ readme.md
@@ -1,8 +1,23 @@
+## Prerequisites
 * clone or download
-* run `npm install`
+* run `npm i`
+* run `npm i aurelia-cli -g`
+
+## Dev & Debug
 * open in VS Code
+* run `au build -w`
 * hit <kbd>F5</kbd>
 
+## Build prerequisites
+* run `npm run init-app`
+* inside `dist-electron` run `npm i`
+
+## Build
+* run `npm run build` to run all the below in sequence
+	* `npm run build-dist` - builds the aureila app for release
+	* `npm run prepare-app` - prepares sources for electron executable build
+	* `npm run build-exe` - builds the electron executable inside `dist-app` folder
 
 ## TODO
 * templates editor
+* detect process end -> reflect in state
